6e0ab48abda8ac2c7448dfa36e1c7281de5e6657
Diego Mesa <diegomesa59@gmail.com>
2025-12-29 15:14:59 -0500

    selectMulti

--- registry/helisa/components/data-table/data-table-column-header.tsx
+++ registry/helisa/components/data-table/data-table-column-header.tsx
@@ -16,8 +16,10 @@ import {
   EyeOffIcon,
 } from "lucide-react";
 
-interface DataTableColumnHeaderProps<TData, TValue>
-  extends React.HTMLAttributes<HTMLDivElement> {
+interface DataTableColumnHeaderProps<
+  TData,
+  TValue,
+> extends React.HTMLAttributes<HTMLDivElement> {
   column: Column<TData, TValue>;
   title: string;
 }
